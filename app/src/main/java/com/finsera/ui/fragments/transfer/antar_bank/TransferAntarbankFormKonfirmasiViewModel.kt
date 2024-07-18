56f9652cd649674fa79998bd0af16193717d7305
lailadwilatifa <lailadwilatifa2@gmail.com>
2024-07-18 16:05:05 +0700

    update ui transfer antar bank, add ui konfirmasi transaksi

---
+++ app/src/main/java/com/finsera/ui/fragments/transfer/antar_bank/TransferAntarbankFormKonfirmasiViewModel.kt
@@ -0,0 +1,7 @@
+package com.finsera.ui.fragments.transfer.antar_bank
+
+import androidx.lifecycle.ViewModel
+
+class TransferAntarbankFormKonfirmasiViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
