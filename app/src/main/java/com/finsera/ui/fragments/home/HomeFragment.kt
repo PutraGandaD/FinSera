ec981fd50edf976c2176190dade847ddfb0b2d70
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2024-07-20 17:25:38 +0700

    Added Bottom Navigation Bar to app (unfinish)
    DONE :
    - Added Material Design 3 Bottom Navigation
    - Disabled Item Active Indicator
    - Added Shadow above Bottom Navigation
    - Customed Font Family and Size for Bottom Navigation
    
    WIP :
    - Change selected item color
    - Differentiate QRIS Button Color / Shape
    
    Reference :
    https://github.com/material-components/material-components-android/blob/master/docs/components/BottomNavigation.md
    https://stackoverflow.com/questions/41650778/android-bottom-navigation-bar-with-drop-shadow
    https://stackoverflow.com/questions/73100046/bottomnavigation-selected-item-background-color-change

--- app/src/main/java/com/finsera/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/finsera/ui/fragments/home/HomeFragment.kt
@@ -5,14 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.finsera.R
+import com.finsera.databinding.FragmentHomeBinding
+import com.finsera.databinding.FragmentLoginBinding
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.menu_navbar_beranda -> {
+                    Toast.makeText(requireActivity(), "Beranda", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_navbar_mutasi -> {
+                    Toast.makeText(requireActivity(), "Mutasi", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_navbar_qris -> {
+                    Toast.makeText(requireActivity(), "QRIS", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_navbar_favorit -> {
+                    Toast.makeText(requireActivity(), "Favorit", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.menu_navbar_akun -> {
+                    Toast.makeText(requireActivity(), "Akun", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
