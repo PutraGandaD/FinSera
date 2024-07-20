6a40cc53af448d6b85255f925d46138b7da326cf
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2024-07-20 23:03:59 +0700

    mutasi logic

--- app/src/main/java/com/finsera/MainActivity.kt
+++ app/src/main/java/com/finsera/MainActivity.kt
@@ -2,10 +2,15 @@ package com.finsera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.finsera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
