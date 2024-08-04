b7c6b6bc96dd7d79b575821a98210afba077b092
PutraGandaD <putraganda.dewata@gmail.com>
2024-08-04 07:13:14 +0700

    feat: Disable Screen Zoom/Display Scale features (Android N+) behavior in the app
    I know, it's better for separate each layout into separate density (hdpi, mdpi, etc)
    But not enough time, i decided to force the app layout into Default scaling.
    So any device display scaling settings won't override the app and made the app layout like a mess.
    
    References (Big thanks to these solutions) =
    https://medium.com/programming-lite/best-practices-for-supporting-android-application-in-multiple-screen-a685afa83493

--- app/src/main/java/com/finsera/MainActivity.kt
+++ app/src/main/java/com/finsera/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.finsera
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.navigation.findNavController
 import com.finsera.databinding.ActivityMainBinding
 
@@ -14,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         this.findNavController(R.id.NavHostFragment)
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        val configuration = newBase.resources.configuration
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val displayMetrics = newBase.resources.displayMetrics
+            if (displayMetrics.densityDpi != DisplayMetrics.DENSITY_DEVICE_STABLE) {
+                // Current density is different from Default Density. Override it
+                configuration.densityDpi = DisplayMetrics.DENSITY_DEVICE_STABLE
+            }
+        }
+        configuration.fontScale = 1.0f
+        val newContext = newBase.createConfigurationContext(configuration)
+        super.attachBaseContext(newContext)
     }
 }
